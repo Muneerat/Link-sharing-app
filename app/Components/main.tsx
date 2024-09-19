b7fc61b30b0b1573378e0c538e6d56e7a148e291
Muneerat <oladipomunirat@gmail.com>
2024-09-19 16:54:05 +0100

    Add: implement the empty link page

--- app/Components/main.tsx
+++ app/Components/main.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import { Phone } from '../asset/icon'
+import React from "react";
+import { Phone, HandPhone } from "../asset/icon";
+import { Button } from "@/components/ui/button";
 
 export const Main = () => {
   return (
-    <div className='flex gap-7'>
-        <div className='bg-white w-2/6 p-5 rounded-md'>
-            <Phone />
+    <div className="md:flex gap-7 py-5 w-full h-">
+      <div className="bg-white md:w-2/5 w-full hidden p-10 rounded-md md:flex justify-center items-center">
+        <Phone />
+      </div>
+      <div className="bg-white md:w-3/5 w-full rounded-md p-10 m-">
+        <h1 className="py-3 font-bold text-2xl text-destructive">
+          Customize your links
+        </h1>
+        <p className="text-border pb-10">
+          Add/edit/remove links below and then share all your profiles with the
+          world!
+        </p>
+        <Button className="w-full bg-white border-primary border text-primary p-6 hover:bg-secondary duration-200 ease-in ">
+          + Add new link
+        </Button>
+        <div className=" bg-background p-12 flex flex-col items-center justify-center my-6">
+          <HandPhone />
+          <div className="max-w-xl text-center py-2">
+            <h1 className="py-3 font-bold text-2xl text-destructive">
+              Let’s get you started
+            </h1>
+            <p className="text-border">
+              Use the “Add new link” button to get started. Once you have more
+              than one link, you can reorder and edit them. We’re here to help
+              you share your profiles with everyone!
+            </p>
+          </div>
         </div>
-        <div className='bg-white w-4/5 rounded-md'>
-         <h1 className=''>Customize your links</h1>
+        <div className="flex justify-end ">
+          <Button className=" bg-primary-foreground text-secondary">Save</Button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- app/Components/navBar.tsx
+++ app/Components/navBar.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Logo from "../asset/logo.png";
 import Image from "next/image";
 import Link from "next/link";
-import { LInkHoverIcon, LinkIcon, ProfileIcon,ProfileHoverIcon } from "../asset/icon";
+import { LInkHoverIcon, LinkIcon, ProfileIcon,ProfileHoverIcon, LogoIcon } from "../asset/icon";
 
 
 export const NavBar = () => {
@@ -15,39 +15,43 @@ export const NavBar = () => {
 
   return (
     <div
-      className={`bg-white flex justify-between rounded-lg py-3 px-2 items-center mb-4`}
+      className={`bg-white flex justify-between rounded-lg py-5 px-5 items-center mb-4`}
     >
       <div className="">
-        <Image src={Logo} alt="Logo" width={140} />
+        <Image src={Logo} alt="Logo" width={140} className="sm:flex hidden" />
+        <div className="flex sm:hidden">
+        <LogoIcon />
+        </div>
       </div>
       <div className="flex justify-between items-center gap-10">
         <Link
           href=""
           className={`flex items-center gap-1 rounded-md text-border font-bold  ${
-            activeLink ? "py-3 px-4 bg-secondary text-primary " : "bg-red-10"
+            activeLink ? "py-3 px-4 bg-secondary text-primary " : "hover:text-primary"
           }`}
           onClick={handleClick}
         >
           {activeLink ?  <LInkHoverIcon /> : <LinkIcon />}
-          Links
+          <p className="sm:flex hidden">Links</p>
+          
         </Link>
         <div className="hover:text-primary">
           <Link
             href=""
             className={`flex items-center gap-1 rounded-md text-border font-bold  ${
-              activeLink ? "py-3 px-4 bg-secondary text-primary " : "bg-red-10"
+              activeLink ? "py-3 px-4 bg-secondary text-primary " : "hover:text-primary"
             }`}
             onClick={handleClick}
           >
              {activeLink ?   <ProfileHoverIcon /> :  <ProfileIcon />}
-            Profile Details
+             <p className="sm:flex hidden">Profile Details</p>
           </Link>
         </div>
       </div>
       <div className="">
         <Link
           href=""
-          className="text-primary flex items-center gap-1 border-primary border rounded-md p-3"
+          className="text-primary flex items-center gap-1 border-primary border rounded-md p-3 hover:bg-secondary duration-200 ease-in-out"
         >
           Preview
         </Link>
